e0f5a188e790b0192cf0f32c17008730844c10ae
Bibin Lopez <bibinlopez36@gmail.com>
2023-07-20 11:48:01 +0530

    completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const connectDB = require('./db/connect')
+const bodyParser = require('body-parser')
 const Message = require('./Model/chatSchema')
 
 const express = require('express')
@@ -8,25 +9,55 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
-app.use(express.json())
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const port = process.env.PORT || 3000
 
-// app.post('/', (req, res) => {
-//    console.log(req.body);
-// })
+app.get('/messages', (req, res) => {
+   Message.find({}).then((messages) => {
+      res.send(messages)
+   })
+})
 
 
-// app.get('/', (req, res) => {
-//    Message.find({}).then((result) => {
-//       // console.log(result);
-//       res.status(200).json(result)
-//    }).catch((error) => {
-//       console.log(error);
-//    })
+app.get('/messages/:user', (req, res) => {
+   const user = req.params.user
+   Message.find({ name: user }).then((messages) => {
+      res.send(messages)
+   })
+})
 
-// })
+
+app.post('/messages', async (req, res) => {
+   try {
+      console.log(req.body, '@@@@');
+      const message = new Message(req.body);
+
+      await message.save()
+      console.log('saved');
+
+      var censored = await Message.findOne({ message: 'badword' });
+      if (censored)
+         await Message.remove({ _id: censored.id })
+      else
+         io.emit('message', req.body);
+      res.sendStatus(200);
+   }
+   catch (error) {
+      res.sendStatus(500);
+      return console.log('error', error);
+   }
+   finally {
+      console.log('Message Posted')
+   }
+
+})
+
+app.post('/delete', (req, res) => {
+   // console.log('this is the route for delete all chats');
+   Message.deleteMany({}).then(() => console.log('deleted')).catch((error) => console.log(error))
+})
 
 
 io.on('connection', () => {
